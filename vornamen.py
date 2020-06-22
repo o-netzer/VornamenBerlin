2d6abcd7898e28ff180d0c65fa345542c9b647b6
Ollie N <owmnetzer@yahoo.de>
2020-06-22 19:32:29 +0200

    added combined "year.csv" to each "year" folder, containing all names of each district of Berlin per year

--- vornamen.py
+++ vornamen.py
@@ -10,16 +10,42 @@ from pathlib import Path
 from pprint import pprint
 
 
-sourcepath = r"D:\PythonScripts20\haeufige-vornamen-berlin\haeufige-vornamen-berlin\data\source"
+############## source path to csv files ##########################
 
-p = Path(sourcepath)
+year = '2012'
 
-dirs = [x for x in p.iterdir() if x.is_dir()]
+############## source path to csv files ##########################
 
-files = []   
-for d in dirs:
-    for f in d.iterdir():
-        if f.name.endswith(('csv')):
-            files.append(f.name)
+# p = Path(sourcepath)
 
-pprint(sorted(files))
+# dirs = [x for x in p.iterdir() if x.is_dir()]
+
+# files = []   
+# for d in dirs:
+#     for f in d.iterdir():
+#         if f.name.endswith(('csv')):
+#             files.append(f.name)
+
+# pprint(sorted(files))
+
+import os
+import glob
+import pandas as pd
+import gc
+
+sourcepath = "D:\Tableau\VornamenBerlin\data\cleaned"
+sourceyear = os.path.join(sourcepath,year)
+
+os.chdir(sourceyear)
+
+extension = 'csv'
+all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+#combine all files in the list
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
+#export to csv
+combined_csv.to_csv(year + ".csv",
+                    index=False,
+                    encoding='utf-8-sig')
+gc.collect()
+
+
