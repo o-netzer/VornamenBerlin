9ca776ef8d89226044289e5e8112c2754ae0cfa3
Ollie N <owmnetzer@yahoo.de>
2020-06-23 11:56:36 +0200

    using script vornamen.py for every year the csv files were combined and added to new folder called union_2012-2019 ready to be used in Tableau

--- vornamen.py
+++ vornamen.py
@@ -5,10 +5,6 @@ Created on Wed Jun  3 18:50:52 2020
 @author: netzer
 """
 
-import csv
-from pathlib import Path
-from pprint import pprint
-
 
 ############## source path to csv files ##########################
 
@@ -17,7 +13,6 @@ year = '2012'
 ############## source path to csv files ##########################
 
 # p = Path(sourcepath)
-
 # dirs = [x for x in p.iterdir() if x.is_dir()]
 
 # files = []   
@@ -25,7 +20,6 @@ year = '2012'
 #     for f in d.iterdir():
 #         if f.name.endswith(('csv')):
 #             files.append(f.name)
-
 # pprint(sorted(files))
 
 import os
@@ -34,18 +28,37 @@ import pandas as pd
 import gc
 
 sourcepath = "D:\Tableau\VornamenBerlin\data\cleaned"
+os.chdir(sourcepath)
+combined_years = 'union_2012-2019'
+if not os.path.exists(os.path.join(sourcepath,combined_years)):
+    os.mkdir(os.path.join(sourcepath,combined_years))
+
+
 sourceyear = os.path.join(sourcepath,year)
+
 
 os.chdir(sourceyear)
 
+
 extension = 'csv'
+combined_csv = pd.DataFrame()
 all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
 #combine all files in the list
-combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
-#export to csv
+for file in all_filenames:
+    df = pd.read_csv(file,
+                header=0,
+                names = ['Vorname', 'Anzahl', 'Geschlecht']) # + Position ab 2017
+    df['Jahr']=year
+    df['Bezirk']=file
+    
+    combined_csv = combined_csv.append(df)
+    
+os.chdir(os.path.join(sourcepath,combined_years))
+
 combined_csv.to_csv(year + ".csv",
-                    index=False,
-                    encoding='utf-8-sig')
-gc.collect()
+                index=False,
+                encoding='utf-8-sig')
+
+
 
 
